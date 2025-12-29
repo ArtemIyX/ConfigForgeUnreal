882c53c1b8983b4fa4a3eb4be4a7b45d4f15d238
Artem Podorozhko <artem.podorozhko@outlook.com>
2025-12-29 11:41:10 +0200

    Editor module

---
+++ Source/ConfigForgeEditor/ConfigForgeEditor.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class ConfigForgeEditor : ModuleRules
+{
+    public ConfigForgeEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/ConfigForgeEditor/Private/ConfigForgeEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "ConfigForgeEditor.h"
+
+#define LOCTEXT_NAMESPACE "FConfigForgeEditorModule"
+
+void FConfigForgeEditorModule::StartupModule()
+{
+    
+}
+
+void FConfigForgeEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FConfigForgeEditorModule, ConfigForgeEditor)
